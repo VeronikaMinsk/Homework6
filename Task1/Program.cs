a8db6b892bceb598f121466a1638a19e1edca319
Veronika <griva.verona@gmail.com>
2022-09-10 01:43:22 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse); // Работает и так - Split(), зачем тогда кавычки?
+//warning CS8602: Разыменование вероятной пустой ссылки - почему ругается на 5 строку?
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+
+Console.WriteLine($"Числа введеные пользователем: ");
+Console.WriteLine("{0}", string.Join(", ", array)); 
+Console.WriteLine($"Кол-во чисел больше 0: {count}");
